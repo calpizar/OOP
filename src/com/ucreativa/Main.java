e9fcaa58b3b8bc36f209b91c4e6807ee0924dadb
calpizar02 <carmen.alpizartrigueros@fiserv.com>
2021-05-24 19:50:18 -0600

    added main java, updated git.ignore

--- src/com/ucreativa/Main.java
+++ src/com/ucreativa/Main.java
@@ -1,12 +1,16 @@
 package com.ucreativa;
 
 import com.ucreativa.familia.Carmen;
+import com.ucreativa.familia.Xinia;
 
 public class Main {
 
     public static void main(String[] args) {
 
         Carmen one = new Carmen();
+
 	    System.out.println("Hello World");
+
+	    Xinia two = new Xinia("Hola");
     }
 }
---
+++ src/com/ucreativa/familia/Xinia.java
@@ -0,0 +1,10 @@
+package com.ucreativa.familia;
+
+public class Xinia {
+
+    public Xinia(String saludo) {
+
+        System.out.println(saludo);
+    }
+
+}
