3680bff4c788367575d9dd32998b98e2f3fa27da
calpizar02 <carmen.alpizartrigueros@fiserv.com>
2021-06-03 20:42:26 -0600

    Trabajo en clase #8 - Main modificado para capturar los inputs

--- src/com/ucreativa/Main.java
+++ src/com/ucreativa/Main.java
@@ -2,28 +2,58 @@ package com.ucreativa;
 
 import com.ucreativa.vacunacion.entities.Amigo;
 import com.ucreativa.vacunacion.entities.Familiar;
+import com.ucreativa.vacunacion.entities.Persona;
 import com.ucreativa.vacunacion.entities.Vacunas;
 
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Familiar mama = new Familiar ("Xinia", "324192",55,false,"Mama");
+        Scanner in = new Scanner(System.in);
 
-        Familiar hermano = new Familiar ("Javier","12332325",22, false,"Hermano");
+        //create data base
+        List<Vacunas> db = new ArrayList<>();
 
-        Amigo compa = new Amigo ("Juana", "12134325", 30, false,"Amigo","juana.perez");
+        while (true) {
+            String nombre, cedula, edad, riesgo, isAmigo, relacion, facebook, parentesco, marca, print;
+            Persona persona;
+            System.out.println("Nombre: ");
+            nombre = in.nextLine();
+            System.out.println("Cedula: ");
+            cedula = in.nextLine();
+            System.out.println("Edad: ");
+            edad = in.nextLine();
+            System.out.println("Riesgo (S/N): ");
+            riesgo = in.nextLine();
+            System.out.println("Amigo(A)/Familiar(F):");
+            isAmigo = in.nextLine();
+            if (isAmigo.equals("A")) {
+                System.out.println("Relacion: ");
+                relacion = in.nextLine();
+                System.out.println("Facebook: ");
+                facebook = in.nextLine();
+                persona = new Amigo(nombre, cedula, Integer.parseInt(edad), riesgo.equals("S"), relacion, facebook);
+            } else {
+                System.out.println("Parentesco: ");
+                parentesco = in.nextLine();
+                persona = new Familiar(nombre, cedula, Integer.parseInt(edad), riesgo.equals("S"), parentesco);
+            }
+            System.out.println("Vacuna -- Marca: ");
+            marca = in.nextLine();
+            db.add(new Vacunas(persona, marca, new Date()));
 
-        SimpleDateFormat f = new SimpleDateFormat("dd/MM/yyyy");
-        List <Vacunas> vacunas = new ArrayList<>();
-        vacunas.add(new Vacunas(mama,"Pfizer", new Date()));
-        vacunas.add(new Vacunas(hermano,"Pfizer", new Date()));
-        vacunas.add(new Vacunas(compa,"Pfizer", new Date()));
+            System.out.println("Quiere imprimir Lista (S)?");
+            print = in.nextLine();
+            if (print.equals("S")) {
+                for (Vacunas item : db) {
+                    System.out.println(item.getPersona().getNombre() + " vacunado el " + item.getFecha());
+                }
+            }
+        }
     }
 }
